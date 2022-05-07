c554ba7af1e868ae004501f8b033764d087c111f
santospgs <paulo.g.s.santos@gmail.com>
2022-05-07 15:04:35 -0300

    Stage 5 - finalização e conclusão do projeto

--- main.js
+++ main.js
@@ -5,7 +5,44 @@ function onScroll(){
     showNavOnScroll()
     showBackToTopButtonOnScroll()
     changeBackgroundBakToTopButton()
+
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
 };
+
+function activateMenuAtCurrentSection(section){
+
+    // linha que determina o centro da viewport
+    const targetLine = scrollY + (innerHeight / 2)
+    
+    //verificar se a seção passou da linha
+    //quais dados vou precisar?
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    
+    // verifica se o scroll passou do centro da seção 
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+    // indica o fim da seção
+    const sectionEndsAt = sectionTop + sectionHeight
+
+    // verifica se o fim da seção passou da targetline    
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+    // limite da seção
+    const sectionBoudaries = sectionTopReachOrPassedTargetLine &&  !sectionEndPassedTargetLine
+
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+    if(sectionBoudaries){
+        menuElement.classList.add('active')
+    }
+    
+}
 
 function showNavOnScroll(){
     if (scrollY > 0){
@@ -57,3 +94,8 @@ ScrollReveal({
     #contact header,
     #contact .content`
 );
+
+/* let sections = document.body.querySelectorAll('section')
+sections.forEach(function(element){
+    activateMenuAtCurrentSection(element.id)
+}) */
